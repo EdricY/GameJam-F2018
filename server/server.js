1d49d7a11f2d5d74dd05699310bd7a52450c1110
eyu <eyu@cse21803.cs.local>
2018-11-03 09:34:57 -0400

    multiplayer started

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server( { port : 8191 } );
 console.log("Running game server on port " + wss.options.port)
 
+const SERVER_PLAYING = -4
+const SERVER_FULL = -3
 const PLAYERLIST = -2;
 const READY = -1;
 const CONNECT = 0;
@@ -10,14 +12,34 @@ const BOMB = 2;
 
 var playing = false;
 var camerax = 0;
-var cameravx = .5;
+var cameravx = 2;
+
+var f_ord = [];
+var players = [];
+var bombs = [];
+
+var W = 768
+var H = 512
+
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(data) {
         data = JSON.parse(data);
-        console.log(data)
+        // console.log(data)
         if (data.type == CONNECT) {
-            ws.id = wss.clients.size;
+            if (playing){
+                console.log("Connection refused - already playing");
+                ws.send(newPacket(SERVER_PLAYING, null))
+                ws.close();
+                return;
+            } else if (wss.clients.size > 4){
+                console.log("Connection refused - server full");
+                ws.send(newPacket(SERVER_FULL, null))
+                ws.close();
+                return;
+            } 
+
+            ws.id = wss.clients.size - 1;
             if (wss.clients.size);
             
             if (!data.obj.name || data.obj.name.trim().length === 0)
@@ -28,20 +50,50 @@ wss.on('connection', function connection(ws) {
             return;
         } else if (data.type == READY) {
             ws.ready = data.obj;
-            broadcastPlayerReadyList()
+            broadcastPlayerReadyList();
+            
             if (allReady()){
                 console.log("game start")
                 playing = true;
-                sendToAll(READY, null);
+                f_ord = generateFrameOrder();
+                wss.clients.forEach(function each(client) {
+                    if (client.readyState === WebSocket.OPEN) {
+                        let startinfo = {
+                            id: client.id,
+                            f_ord: f_ord,
+                            x: 100 + client.id * 150,
+                        }
+                        client.send(newPacket(READY, startinfo));
+                    }
+                });
                 return
             }
+        } else if (data.type == TICK) {
+            players[ws.id] = data.obj;
         }
     });
     ws.on('close', function closing(data) {
         console.log('Disconnect: ' + ws.name);
+        if (wss.clients.size <= 0) {
+            playing = false;
+            resetServer()
+        }
     });
 });
 
+function resetServer() {
+
+}
+
+function generateFrameOrder() {
+    let numFrames = 6;
+    let order = []
+    order.push(0)
+    for (let i = 0; i < 1; i++) {
+        order.push(Math.floor(Math.random() * numFrames))
+    }
+    return order;
+}
 
 function broadcastPlayerReadyList() {
     let playerlist = []
@@ -83,7 +135,16 @@ function Packet(type, obj) {
 }
 
 function tick() {
-    camerax += cameravx
+    if (playing) {
+        camerax += cameravx;
+
+        sendToAll(TICK, {
+            cx: camerax,
+            p: players,
+            b: bombs
+        })
+    }
+
 }
 
 setInterval(tick, 10);
